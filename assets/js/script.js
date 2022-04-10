e9322216d80351b128932adce2eea73107902e15
gordonbates <gordonbatesofficial@gmail.com>
2022-04-10 09:11:23 +0000

    Add tie, rock, papper, scissors function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,44 @@ options.forEach((option) => {
         let playerInput = this.thyisContent;
 
         let computerOptions = ["Rock", "Paper", "Scissors"];
-        let computerInput = cOptions[Math.floor(Math.random() * 3)];
+        let computerInput = computerOptions[Math.floor(Math.random() * 3)];
 
     });
 });
+
+function compareInputs(playerInput, computerInput) {
+    let currentMatch = `${playerInput} vs ${computerInput}`;
+
+    // Draw Game
+    if (playerInput === computerInput) {
+        alert(`${currentMatch} Draw Game!`);
+        return;
+    }
+}
+
+// Rock
+if (playerInput === "Rock") {
+    if (computerInput === "Scissors") {
+        alert(`${currentMatch} = You Win!`);
+    } else {
+        alert(`${currentMatch} = Computer Wins!`);
+    }
+}
+}
+// Paper
+else if (playerInput === "Paper") {
+    if (computerInput === "Rock") {
+        alert(`${currentMatch} = You Win!`);
+    } else {
+        alert(`${currentMatch} = Computer Wins!`);
+    }
+}
+// Scissors
+else {
+    if (computerInput === "Paper") {
+        alert(`${currentMatch} = You Win!`);
+    } else {
+        alert(`${currentMatch} = Computer Wins!`);
+    }
+}
+}
