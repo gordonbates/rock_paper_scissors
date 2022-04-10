121d6fde83168dfa2041c9a16bd6a0b8c0b7a894
gordonbates <gordonbatesofficial@gmail.com>
2022-04-10 09:48:43 +0000

    Add update score and check winner functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ options.forEach((option) => {
         let computerOptions = ["Rock", "Paper", "Scissors"];
         let computerInput = computerOptions[Math.floor(Math.random() * 3)];
 
+        compareInputs(playerInput, computerInput);
+        updateScore(); 
+        if (checkWinner()) {
+            playerScore = computerScore = 0;
+            updateScore();
+          }
+
     });
 });
 
@@ -19,30 +26,54 @@ function compareInputs(playerInput, computerInput) {
         return;
     }
 }
-
-// Rock
 if (playerInput === "Rock") {
     if (computerInput === "Scissors") {
-        alert(`${currentMatch} = You Win!`);
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
     } else {
-        alert(`${currentMatch} = Computer Wins!`);
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
     }
-}
-}
-// Paper
-else if (playerInput === "Paper") {
+  }
+  //Check for Paper
+  else if (playerInput === "Paper") {
     if (computerInput === "Rock") {
-        alert(`${currentMatch} = You Win!`);
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
     } else {
-        alert(`${currentMatch} = Computer Wins!`);
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
     }
-}
-// Scissors
-else {
+  }
+  //Check for Scissors
+  else {
     if (computerInput === "Paper") {
-        alert(`${currentMatch} = You Win!`);
+      alert(`${currentMatch} = You Win`);
+      playerScore++;
     } else {
-        alert(`${currentMatch} = Computer Wins!`);
+      alert(`${currentMatch} = Computer Wins`);
+      computerScore++;
     }
+  }
 }
-}
+
+
+let options = document.querySelectorAll(".options");
+let playerScore = 0;
+let computerScore = 0;
+
+options.forEach((option) => {
+  // ...
+});
+
+function checkWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+      let winner =
+        playerScore === 5
+          ? "Congratulations! You are the winner! "
+          : "Game Over! You lost";
+      alert(winner);
+      return true;
+    }
+    return false;
+  }
