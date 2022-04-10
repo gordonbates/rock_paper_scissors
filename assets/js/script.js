790e74f1f5865b10b478cf1395e9e2e67fc6a950
gordonbates <gordonbatesofficial@gmail.com>
2022-04-10 12:36:58 +0000

    Add RPS images as inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,74 @@
 let options = document.querySelectorAll(".options");
+let playerScore = 0;
+let computerScore = 0;
 
 options.forEach((option) => {
     option.addEventListener("click", function () {
-        let playerInput = this.thyisContent;
+        let playerInput = this.textContent;
 
         let computerOptions = ["Rock", "Paper", "Scissors"];
         let computerInput = computerOptions[Math.floor(Math.random() * 3)];
 
         compareInputs(playerInput, computerInput);
-        updateScore(); 
+        updateScore();
         if (checkWinner()) {
             playerScore = computerScore = 0;
             updateScore();
-          }
-
+        }
     });
 });
-
+// Draw
 function compareInputs(playerInput, computerInput) {
     let currentMatch = `${playerInput} vs ${computerInput}`;
-
-    // Draw Game
     if (playerInput === computerInput) {
-        alert(`${currentMatch} Draw Game!`);
+        alert(`${currentMatch} is a Draw`);
         return;
     }
+    // Check for Rock
+    if (playerInput === "Rock") {
+        if (computerInput === "Scissors") {
+            alert(`${currentMatch} = You Win`);
+            playerScore++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            computerScore++;
+        }
+    }
+    //Check for Paper
+    else if (playerInput === "Paper") {
+        if (computerInput === "Rock") {
+            alert(`${currentMatch} = You Win`);
+            playerScore++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            computerScore++;
+        }
+    }
+    //Check for Scissors
+    else {
+        if (computerInput === "Paper") {
+            alert(`${currentMatch} = You Win`);
+            playerScore++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            computerScore++;
+        }
+    }
 }
-if (playerInput === "Rock") {
-    if (computerInput === "Scissors") {
-      alert(`${currentMatch} = You Win`);
-      playerScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      computerScore++;
-    }
-  }
-  //Check for Paper
-  else if (playerInput === "Paper") {
-    if (computerInput === "Rock") {
-      alert(`${currentMatch} = You Win`);
-      playerScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      computerScore++;
-    }
-  }
-  //Check for Scissors
-  else {
-    if (computerInput === "Paper") {
-      alert(`${currentMatch} = You Win`);
-      playerScore++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      computerScore++;
-    }
-  }
+
+function updateScore() {
+    document.getElementById("player-score").textContent = playerScore;
+    document.getElementById("computer-score").textContent = computerScore;
 }
-
-
-let options = document.querySelectorAll(".options");
-let playerScore = 0;
-let computerScore = 0;
-
-options.forEach((option) => {
-  // ...
-});
 
 function checkWinner() {
     if (playerScore === 5 || computerScore === 5) {
-      let winner =
-        playerScore === 5
-          ? "Congratulations! You are the winner! "
-          : "Game Over! You lost";
-      alert(winner);
-      return true;
+        let winner =
+            playerScore === 5 ?
+            "🥳Congratulations! You win the game!" :
+            "☠️Game Over!! You lost the game!";
+        alert(winner);
+        return true;
     }
     return false;
-  }
+}
