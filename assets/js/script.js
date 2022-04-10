7704352086aed31d1b5cdc2e203cd2f7197ad992
gordonbates <gordonbatesofficial@gmail.com>
2022-04-10 08:57:10 +0000

    Add computer options but using random math

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 let options = document.querySelectorAll(".options");
 
-    options.forEach((option) => {
-      option.addEventListener("click", function () {
-        
-      });
+options.forEach((option) => {
+    option.addEventListener("click", function () {
+        let playerInput = this.thyisContent;
+
+        let computerOptions = ["Rock", "Paper", "Scissors"];
+        let computerInput = cOptions[Math.floor(Math.random() * 3)];
+
     });
+});
